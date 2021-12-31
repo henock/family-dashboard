bea66c82fafbe9ec13ef91d0cf470ee764aad8fb
henock <henock@zewde.com>
2021-12-31 17:23:17 +0000

    Bugfix: Removing mkdir /etc/periodic/1min which was alraedy created in Dockerfile.

--- startup-crond.sh
+++ startup-crond.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 crontab -l > /current_cron
-mkdir /etc/periodic/1min
 echo '*       *       *       *       *       /etc/periodic/1min/continuously-deploy-website.sh' >> /current_cron 2>&1
 crontab current_cron
 rm current_cron
