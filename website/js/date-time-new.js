80792653c94b5f48ffc566d4c9e7c67d4710c95c
Henock Zewde <henock@zewde.com>
2024-10-07 11:55:33 +0100

    Adding nowPlusSeconds()

--- website/js/date-time-new.js
+++ website/js/date-time-new.js
@@ -1,9 +1,24 @@
 
+const A_SECOND				=  1000;
+const A_MINUTE				= A_SECOND * 60;
+const FIVE_MINUTES 			= A_MINUTE * 5
+const TEN_MINUTES 			= A_MINUTE * 10
+const FIFTEEN_MINUTES		= A_MINUTE * 15
+const HALF_AN_HOUR 			= A_MINUTE * 30
+const AN_HOUR				= A_MINUTE * 60;
+const TWELVE_HOURS			= AN_HOUR * 12;
+const TWENTY_FOUR_HOURS		= AN_HOUR * 24;
+
+
 var dashboardDate = {}
 
 function setDashboardDate( otherDate ){
     dashboardDate = (otherDate && otherDate instanceof Date? otherDate: new Date());
     return dashboardDate;
+}
+
+function nowPlusSecond( seconds ){
+    return calculateDateFor( "now+" + seconds + "s" );
 }
 
 function calculateDateFor( text ){
@@ -20,13 +35,13 @@ function calculateDateFor( text ){
 
     switch( suffix ){
         case "s":
-            multiplier = 1000;
+            multiplier = A_SECOND;
             break;
         case "m":
-            multiplier = 60000;
+            multiplier = A_MINUTE;
             break;
         case "h":
-            multiplier = 36000;
+            multiplier = AN_HOUR;
             break;
     }
 
--- website/js/tests/date-time-testing.js
+++ website/js/tests/date-time-testing.js
@@ -1,26 +1,10 @@
-function calculateDateForTest(allTestResults){
 
-    function helper( comment, params, expected ){
-        allTestResults.push(
-            runUnitTest( "calculateDateFor", comment, params, expected,
-                         displayStringified, dateTimeComparatorInsensitive));
-    }
-
-    const SECOND = 1000;
-    const TWO_SECOND = 2000;
-    const TEN_SECOND = 10000;
-    const ONE_MIN = 60000;
-    const TEN_MIN = 60000;
-    const ONE_HOUR = 36000;
-    helper( "now+2s", ["now+2s"], new Date(new Date().getTime()+TWO_SECOND));
-    helper( "now+10s", ["now+10s"], new Date(new Date().getTime()+TEN_SECOND));
-    helper( "now-10s", ["now-10s"], new Date(new Date().getTime()-TEN_SECOND));
-    helper( "now+1m", ["now+1m"], new Date(new Date().getTime()+ONE_MIN));
-    helper( "now-1m", ["now-1m"], new Date(new Date().getTime()-ONE_MIN));
-    helper( "now+2h", ["now+2h"], new Date(new Date().getTime()+ONE_HOUR+ONE_HOUR));
-    helper( "now-1h", ["now-1h"], new Date(new Date().getTime()-ONE_HOUR));
-    helper( "now+24h", ["now+24h"], new Date(new Date().getTime()+(24*ONE_HOUR)));
-}
+const ONE_SECOND = 1000;
+const TWO_SECOND = 2 * ONE_SECOND;
+const TEN_SECOND = 10 * ONE_SECOND;
+const ONE_MIN = 60 * ONE_SECOND;
+const TEN_MIN = 10 * ONE_MIN;
+const ONE_HOUR = 60 * ONE_MIN;
 
 
 function runUnitTestsForDateTime(){
@@ -44,5 +28,37 @@ function runUnitTestsForDateTime(){
 
     calculateDateForTest(allTestResults);
 
+    allTestResults.push(
+        runUnitTest( "nowPlusSecond", "now+10s",
+                    [10],
+                    new Date(new Date().getTime()+TEN_SECOND),
+                    displayStringified , dateTimeComparatorInsensitive));
+
+    allTestResults.push(
+        runUnitTest( "nowPlusSecond", "now-10s",
+                    [-10],
+                    new Date(new Date().getTime()-TEN_SECOND),
+                    displayStringified , dateTimeComparatorInsensitive));
+
      return allTestResults;
+}
+
+
+
+
+function calculateDateForTest(allTestResults){
+
+    function helper( comment, params, expected ){
+        allTestResults.push(
+            runUnitTest( "calculateDateFor", comment, params, expected,
+                         displayStringified, dateTimeComparatorInsensitive));
+    }
+    helper( "now+2s", ["now+2s"], new Date(new Date().getTime()+TWO_SECOND));
+    helper( "now+10s", ["now+10s"], new Date(new Date().getTime()+TEN_SECOND));
+    helper( "now-10s", ["now-10s"], new Date(new Date().getTime()-TEN_SECOND));
+    helper( "now+1m", ["now+1m"], new Date(new Date().getTime()+ONE_MIN));
+    helper( "now-1m", ["now-1m"], new Date(new Date().getTime()-ONE_MIN));
+    helper( "now+2h", ["now+2h"], new Date(new Date().getTime()+ONE_HOUR+ONE_HOUR));
+    helper( "now-1h", ["now-1h"], new Date(new Date().getTime()-ONE_HOUR));
+    helper( "now+24h", ["now+24h"], new Date(new Date().getTime()+(24*ONE_HOUR)));
 }
