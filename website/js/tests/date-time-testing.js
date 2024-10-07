57bd58c20bcb81296e56fb96465248a89f3880ea
Henock Zewde <henock@zewde.com>
2024-10-08 00:39:32 +0100

    Refactoring date-time-testing.js to use refactored framework.

--- website/js/tests/date-time-testing.js
+++ website/js/tests/date-time-testing.js
@@ -8,71 +8,144 @@ const ONE_HOUR = 60 * ONE_MIN;
 
 
 function runUnitTestsForDateTime(){
-    var allTestResults = [];
+    const DATE_A = new Date(1000000000000);
+    const DATE_B = new Date(2000000000000);
+    setDashboardDate(DATE_A); // Fix the clock for all time based tests
 
-    allTestResults.push(addTestGrouping( "Date and time functions tests"));
-
-
-    allTestResults.push(
-        runUnitTest( "setDashboardDate", "should set the current clock (dashboard date) to now",
-                    [],
-                    new Date(),
-                    displayStringified , dateTimeComparatorInsensitive));
-
-
-    let aDate = new Date(2000000000000);
-
-    allTestResults.push(
-        runUnitTest( "setDashboardDate", "should set the current clock (dashboard date) to time passed in",
-                    [aDate],
-                    aDate,
-                    displayStringified , dateTimeComparatorExact));
-
-
-    calculateDateForTest(allTestResults);
-
-    allTestResults.push(
-        runUnitTest( "nowPlusSecond", "now+10s", [10], "2001-09-09T01:46:50.000Z",
-                        displayStringified , stringifyComparator));
-
-    allTestResults.push(
-        runUnitTest( "nowPlusSecond", "now-10s", [-10], "2001-09-09T01:46:30.000Z",
-                        displayStringified , stringifyComparator));
-
-    allTestResults.push(
-        runUnitTest( "getPaddedTimeSeconds", "get time from dashboard", [], "02:46:40",
-                        displayStringified , stringifyComparator));
-
-    allTestResults.push(
-        runUnitTest( "getPaddedTimeSeconds", "get time from dashboard", [aDate], "04:33:20",
-                        displayStringified , stringifyComparator));
-
-    allTestResults.push(
-        runUnitTest( "getPaddedTimeMinutes", "get time from dashboard", [], "02:46",
-                        displayStringified , stringifyComparator));
-
-    allTestResults.push(
-        runUnitTest( "getPaddedTimeMinutes", "get time from dashboard", [aDate], "04:33",
-                        displayStringified , stringifyComparator));
-
-     return allTestResults;
+    return runUnitTestSuite({
+        groupName: "Date and time functions tests",
+        tests: [
+            {
+                functionUnderTest: "setDashboardDate",
+                comment: "should set the current clock (dashboard date) to now",
+                parameters: [],
+                expectedResult: new Date(),
+                displayFormatter: displayStringified,
+                comparator: dateTimeComparatorInsensitive,
+                afterTest: function(){ setDashboardDate(DATE_A);} //reset the Dashboard Date after the test
+            },
+            {
+                functionUnderTest: "setDashboardDate",
+                comment: "should set the current clock (dashboard date) to time passed in",
+                parameters: [DATE_B],
+                expectedResult: DATE_B,
+                displayFormatter: displayStringified,
+                comparator: dateTimeComparatorExact,
+                afterTest: function(){ setDashboardDate(DATE_A);}  //reset the Dashboard Date after the test
+            },
+            {
+                functionUnderTest: "calculateDateFor",
+                comment: "now+2s",
+                parameters: ["now+2s"],
+                expectedResult: "2001-09-09T01:46:42.000Z",
+                displayFormatter: displayStringified,
+                comparator: stringifyComparator
+            },
+            {
+                functionUnderTest: "calculateDateFor",
+                comment: "now+10s",
+                parameters: ["now+10s"],
+                expectedResult: "2001-09-09T01:46:50.000Z",
+                displayFormatter: displayStringified,
+                comparator: stringifyComparator
+            },
+            {
+                functionUnderTest: "calculateDateFor",
+                comment: "now-10s",
+                parameters: ["now-10s"],
+                expectedResult: "2001-09-09T01:46:30.000Z",
+                displayFormatter: displayStringified,
+                comparator: stringifyComparator
+            },
+            {
+                functionUnderTest: "calculateDateFor",
+                comment: "now+1m",
+                parameters: ["now+1m"],
+                expectedResult: "2001-09-09T01:47:40.000Z",
+                displayFormatter: displayStringified,
+                comparator: stringifyComparator
+            },
+            {
+                functionUnderTest: "calculateDateFor",
+                comment: "now-1m",
+                parameters: ["now-1m"],
+                expectedResult: "2001-09-09T01:45:40.000Z",
+                displayFormatter: displayStringified,
+                comparator: stringifyComparator
+            },
+            {
+                functionUnderTest: "calculateDateFor",
+                comment: "now+2h",
+                parameters: ["now+2h"],
+                expectedResult: "2001-09-09T03:46:40.000Z",
+                displayFormatter: displayStringified,
+                comparator: stringifyComparator
+            },
+            {
+                functionUnderTest: "calculateDateFor",
+                comment: "now-1h",
+                parameters: ["now-1h"],
+                expectedResult: "2001-09-09T00:46:40.000Z",
+                displayFormatter: displayStringified,
+                comparator: stringifyComparator
+            },
+            {
+                functionUnderTest: "calculateDateFor",
+                comment: "now+24h",
+                parameters: ["now+24h"],
+                expectedResult: "2001-09-10T01:46:40.000Z",
+                displayFormatter: displayStringified,
+                comparator: stringifyComparator
+            },
+            {
+                functionUnderTest: "nowPlusSecond",
+                comment: "now+10s",
+                parameters: [10],
+                expectedResult: "2001-09-09T01:46:50.000Z",
+                displayFormatter: displayStringified ,
+                comparator: stringifyComparator
+            },
+            {
+                functionUnderTest: "nowPlusSecond",
+                comment: "now-10s",
+                parameters: [-10],
+                expectedResult: "2001-09-09T01:46:30.000Z",
+                displayFormatter: displayStringified,
+                comparator: stringifyComparator
+            },
+            {
+                functionUnderTest: "getPaddedTimeSeconds",
+                comment: "get time from dashboard",
+                parameters: [],
+                expectedResult: "02:46:40",
+                displayFormatter: displayStringified,
+                comparator: stringifyComparator
+            },
+            {
+                functionUnderTest: "getPaddedTimeSeconds",
+                comment: "get time from dashboard",
+                parameters: [DATE_B],
+                expectedResult: "04:33:20",
+                displayFormatter: displayStringified,
+                comparator: stringifyComparator
+            },
+            {
+                functionUnderTest: "getPaddedTimeMinutes",
+                comment: "get time from dashboard",
+                parameters: [],
+                expectedResult: "02:46",
+                displayFormatter: displayStringified,
+                comparator: stringifyComparator
+            },
+            {
+                functionUnderTest: "getPaddedTimeMinutes",
+                comment: "get time from dashboard",
+                parameters: [DATE_B],
+                expectedResult: "04:33",
+                displayFormatter: displayStringified,
+                comparator: stringifyComparator
+            }
+        ]
+    });
 }
 
-
-
-
-function calculateDateForTest(allTestResults){
-
-    function helper( comment, params, expected ){
-        allTestResults.push( runUnitTest( "calculateDateFor", comment, params, expected, displayStringified, stringifyComparator));
-    }
-    setDashboardDate(new Date(1000000000000)); // Fix the clock for all time based tests
-    helper( "now+2s", ["now+2s"], "2001-09-09T01:46:42.000Z");
-    helper( "now+10s", ["now+10s"], "2001-09-09T01:46:50.000Z");
-    helper( "now-10s", ["now-10s"], "2001-09-09T01:46:30.000Z");
-    helper( "now+1m", ["now+1m"], "2001-09-09T01:47:40.000Z");
-    helper( "now-1m", ["now-1m"], "2001-09-09T01:45:40.000Z");
-    helper( "now+2h", ["now+2h"], "2001-09-09T03:46:40.000Z");
-    helper( "now-1h", ["now-1h"], "2001-09-09T00:46:40.000Z");
-    helper( "now+24h", ["now+24h"], "2001-09-10T01:46:40.000Z");
-}
